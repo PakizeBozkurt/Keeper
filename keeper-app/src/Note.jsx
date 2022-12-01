7bb95ba03d2ba3d0f9a19c38e909ae73e331a9b5
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-01 23:43:27 +0000

    Cr

--- keeper-app/src/Note.jsx
+++ keeper-app/src/Note.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Note() {
   return (
     <div className="note">
-      <h1>Title</h1>
-      <p>Content</p>
+      <h1>This is a title</h1>
+      <p>This is a content</p>
     </div>
   );
 }
