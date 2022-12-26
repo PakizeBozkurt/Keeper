4341cb9fc557f23c9c16a3df253f7cdb44659ea3
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-26 09:12:07 +0000

    Using map();

--- keeper-app/src/App.js
+++ keeper-app/src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note  />
+      {/* <Note title="This is a title" content="This is a content" /> */}
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
