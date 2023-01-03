6273a1a2e2b4e00efb3608437d544ad69b389c0d
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-03 20:15:36 +0000

    Updated

--- keeper-app/src/notes.js
+++ keeper-app/src/notes.js
@@ -21,7 +21,7 @@ const notes = [
     key: 4,
     title: "Hardware vs Software",
     content:
-      "What's the difference between hardware and software? You can hit your hardware with a hammer, bur you can only curse at your software.",
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
   },
   {
     key: 5,
