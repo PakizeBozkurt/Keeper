40176f1ec2086547c290dd6f3a5c7af37cd9ef53
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-10 23:36:25 +0100

    Playing around

--- keeper-app/cypress/e2e/notes.cy.ts
+++ keeper-app/cypress/e2e/notes.cy.ts
@@ -2,36 +2,11 @@
 describe("notes managment", () => {
   it("should open and close the new note modal", () => {
     cy.visit("http://localhost:3001/");
-    cy.contains('form').click();
-    cy.get('form').click({force: true});
-  });
-});
-describe('Note component', () => {
-  it('should display note title and content', () => {
-    const note = {
-      id: 1,
-      title: 'Test Note',
-      content: 'This is a test note',
-    };
+    cy.contains('.note').click();
+    cy.get('.note').click({force: true});
+    cy.get('.note').should('not.exist');
 
-    
-
-    cy.get('h1').should('have.text', 'Test Note');
-    cy.get('p').should('have.text', 'This is a test note');
-  });
-
-  it('should call onDelete when delete button is clicked', () => {
-    const onDelete = cy.stub();
-    const note = {
-      id: 1,
-      title: 'Test Note',
-      content: 'This is a test note',
-      onDelete,
-    };
-
-  
-
-    cy.get('button').click();
-    expect(onDelete).to.be.calledWith(1);
+    cy.contains('.note').click();
+    cy.contains('h1').click();
   });
 });
